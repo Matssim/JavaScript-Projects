f23dfeb7c5701abeb5c16282508dbafaea18045f
Mats Simonsen <mats.simonsen@outlook.com>
2024-09-20 11:43:58 -0700

    Uploading Project1_expressions_alert files
    Step 65

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+var A = "This is the first string" //Defines value of variable A
+
+var B = "This is the second alert" //Defines value of variable B
+
+var C = "As I would say\:" //Defines value of variable C
+
+var D = "\"This is a quote\"" //Defines value of variable D
+
+var E = "This is the beginning of the string" + " and this is the end of the string" //Defines variable E, concatenating string values 
+
+window.alert("Hello, World!"); //Displays pop-up of given text upon entering site
+
+window.alert(B); //Displays pop-up upon entering site, calling on variable B
+
+document.write('Hello, World!'); //Displays the given text 
+
+document.write(A); //Displays value of variable A
+
+document.write(C+D); //Concatenates and displays the vaules of variables C and D
+
+document.write(E); //Displays value of variable E
+
+document.write(2 + 2); //Displays value of the given expression
